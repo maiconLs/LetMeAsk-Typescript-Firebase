38572cf635f9593b61d0a228dcb7b9e19d4e2947
Maicon <maiconlsilva10@gmail.com>
2022-01-06 22:21:24 -0300

    commit

---
+++ .history/src/App_20211116204141.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/rooms/new" element={<NewRoom/>} />
+          <Route path="/rooms/:id" element={<Room/>} />
+
+          <Route path="/admin/rooms/:id" element={<AdminRoom/>} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20211117185245.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/rooms/new" element={<NewRoom/>} />
+          <Route path="/rooms/:id" element={<Room/>} />
+
+          <Route path="/admin/rooms/:id" element={<AdminRoom/>} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+// Dependencias:
+
+// yarn add node-sass@^5.0.0
+// yarn add @types/react-router-dom -D   
+// yarn add classnames
+// yarn add react-modal
+
+export default App;
